185d3be430cbbbe1020d1dde0325eb4b83851d1e
Juzzl <jussl2598@gmail.com>
2025-05-24 12:27:33 -0600

    Creacion del metodo de ObtenerEqPorID

---
+++ SetLight/SetLight.Abstracciones/LogicaDeNegocio/Equipment/IObtenerEqPorIDLN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetLight.Abstracciones.ModelosParaUI;
+
+namespace SetLight.Abstracciones.LogicaDeNegocio.Equipment
+{
+    public interface IObtenerEqPorIDLN
+    {
+        EquipmentDto Obtener(int id);
+    }
+}
---
+++ SetLight/SetLight.AccesoADatos/Equipment/ObtenerEqPorID/ObtenerEqPorIDAD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetLight.Abstracciones.AccesoADatos.Equipment.ObtenerEqPorID;
+using SetLight.Abstracciones.ModelosParaUI;
+
+namespace SetLight.AccesoADatos.Equipment.ObtenerEqPorID
+{
+    public class ObtenerEqPorIDAD:IObtenerEqPorIDAD
+    {
+        private Contexto _elContexto;
+
+        public ObtenerEqPorIDAD()
+        {
+            _elContexto = new Contexto();
+        }
+
+        public EquipmentDto Obtener(int id)
+        {
+            EquipmentDto listaDelEquipmentARetornar = (from elEquipment in _elContexto.Equipment
+                                                       where elEquipment.EquipmentId == id
+                                                       select new EquipmentDto
+                                                       {
+                                                           EquipmentId = elEquipment.EquipmentId,
+                                                           EquipmentName = elEquipment.EquipmentName,
+                                                           Brand = elEquipment.Brand,
+                                                           Model = elEquipment.Model,
+                                                           SerialNumber = elEquipment.SerialNumber,
+                                                           Description = elEquipment.Description,
+                                                           Status = elEquipment.Status,
+                                                           CategoryId = elEquipment.CategoryId.ToString()
+
+
+                                                       }).FirstOrDefault();
+            return listaDelEquipmentARetornar;
+
+        }
+
+    }
+}
