9aad224fabf3dc6ce62f71c271e839bfcf98e653
Kcalderon10565 <123605756+Kcalderon10565@users.noreply.github.com>
2025-05-29 13:05:10 -0600

    Creación del método Editar

--- SetLight/SetLight.AccesoADatos/Contexto.cs
+++ SetLight/SetLight.AccesoADatos/Contexto.cs
@@ -14,7 +14,6 @@ namespace SetLight.AccesoADatos
         {
 
         }
-
         public DbSet<EquipmentDA> Equipment { get; set; }
         public DbSet<EqCategoryDA> EqCategory { get; set; }
     }
---
+++ SetLight/SetLight.AccesoADatos/Equipment/EditarEquipment/EditarEquipmentAD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetLight.Abstracciones.AccesoADatos.Equipment.EditarEquipment;
+using SetLight.Abstracciones.ModelosParaUI;
+using SetLight.AccesoADatos.Modelos;
+
+namespace SetLight.AccesoADatos.Equipment.EditarEquipment
+{
+    public class EditarEquipmentAD : IEditarEquipmentAD
+    {
+        private Contexto elContexto;
+        public EditarEquipmentAD()
+        {
+            elContexto = new Contexto();
+        }
+        public int Editar(EquipmentDto elEquipmentParaActualizar)
+        {
+            EquipmentDA elEquipmentEnBaseDeDatos = elContexto.Equipment.Where(elEquipment => elEquipment.EquipmentId == elEquipmentParaActualizar.EquipmentId).FirstOrDefault();
+            elEquipmentEnBaseDeDatos.EquipmentName = elEquipmentParaActualizar.EquipmentName;
+            elEquipmentEnBaseDeDatos.Brand = elEquipmentParaActualizar.Brand;
+            elEquipmentEnBaseDeDatos.Model = elEquipmentParaActualizar.Model;
+            elEquipmentEnBaseDeDatos.SerialNumber = elEquipmentParaActualizar.SerialNumber;
+            elEquipmentEnBaseDeDatos.Description = elEquipmentParaActualizar.Description;
+            elEquipmentEnBaseDeDatos.CategoryId = elEquipmentParaActualizar.CategoryId;
+            int seGuardoElEquipment = elContexto.SaveChanges();
+            return seGuardoElEquipment;
+        }
+    }
+}
