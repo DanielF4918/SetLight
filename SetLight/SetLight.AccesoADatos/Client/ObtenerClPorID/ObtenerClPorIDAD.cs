76c5111b648f5fb10a3aa2357cbb9e8387a4983c
Juzzl <jussl2598@gmail.com>
2025-06-11 18:41:28 -0600

    Creacion de Historial de ordenes para el modulo de clientes

---
+++ SetLight/SetLight.AccesoADatos/Client/ObtenerClPorID/ObtenerClPorIDAD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using SetLight.Abstracciones.AccesoADatos.Client.ObtenerClPorId;
+using SetLight.Abstracciones.ModelosParaUI;
+
+namespace SetLight.AccesoADatos.Clientes.ObtenerClPorID
+{
+    public class ObtenerClPorIDAD : IObtenerClPorIDAD
+    {
+        private Contexto _elContexto;
+
+        public ObtenerClPorIDAD()
+        {
+            _elContexto = new Contexto();
+        }
+
+        public ClientDto Obtener(int id)
+        {
+            ClientDto clienteARetornar = (from c in _elContexto.Clients
+                                          where c.ClientId == id
+                                          select new ClientDto
+                                          {
+                                              ClientId = c.ClientId,
+                                              FirstName = c.FirstName,
+                                              LastName = c.LastName,
+                                              Phone = c.Phone,
+                                              Email = c.Email
+                                          }).FirstOrDefault();
+
+            return clienteARetornar;
+        }
+    }
+}
---
+++ SetLight/SetLight.LogicaDeNegocio/Client/ObtenerClPorIDLN/ObtenerClPorIDLN.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetLight.Abstracciones.AccesoADatos.Client.ObtenerClPorId;
+using SetLight.Abstracciones.LogicaDeNegocio.Client.ObtenerClPorId;
+using SetLight.Abstracciones.ModelosParaUI;
+using SetLight.AccesoADatos.Clientes.ObtenerClPorID;
+
+namespace SetLight.LogicaDeNegocio.Client.ObtenerClPorIDLN
+{
+    public class ObtenerClPorIDLN : IObtenerClPorIDLN
+    {
+        private IObtenerClPorIDAD _obtenerClPorIDAD;
+
+        public ObtenerClPorIDLN()
+        {
+            _obtenerClPorIDAD = new ObtenerClPorIDAD();
+        }
+
+        public ClientDto Obtener(int id)
+        {
+            ClientDto client = _obtenerClPorIDAD.Obtener(id);
+            return client;
+        }
+    }
+}
