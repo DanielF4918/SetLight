13fa603465d1a10bb5b290621405de2f55f006ba
Juzzl <jussl2598@gmail.com>
2025-06-16 20:08:05 -0600

    Implementación del metodo de editar para el modulo de ordenes

---
+++ SetLight/SetLight.LogicaDeNegocio/rentalorder/EditRentalOrder/EditRentalOrderLN.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SetLight.Abstracciones.AccesoADatos.RentalOrder.EditRentalOrder;
+using SetLight.Abstracciones.LogicaDeNegocio.rentalorder.EditRentalOrder;
+using SetLight.Abstracciones.ModelosParaUI;
+using SetLight.AccesoADatos.rentalorder.EditRentalOrder;
+
+namespace SetLight.LogicaDeNegocio.rentalorder.EditRentalOrder
+{
+    public class EditRentalOrderLN:IEditRentalOrderLN
+    {
+        private IEditRentalOrderAD _actualizarRentalOrder;
+
+        public EditRentalOrderLN()
+        {
+            _actualizarRentalOrder = new EditRentalOrderAD();
+        }
+
+        public int Actualizar(RentalOrderDto rentalOrderParaActualizar)
+        {
+
+            return _actualizarRentalOrder.Editar(rentalOrderParaActualizar);
+        }
+    }
+}
