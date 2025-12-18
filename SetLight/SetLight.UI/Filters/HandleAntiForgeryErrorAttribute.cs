5ec3cf9bffa3141e3b45eb56be7413a95f88b663
Juzzl <jussl2598@gmail.com>
2025-12-18 10:40:58 -0600

    Anti-Forgery Token

---
+++ SetLight/SetLight.UI/Filters/HandleAntiForgeryErrorAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace SetLight.UI.Filters
+{
+    public class HandleAntiForgeryErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.Exception is HttpAntiForgeryException)
+            {
+                // Evita la pantalla amarilla
+                filterContext.ExceptionHandled = true;
+
+                // Mensaje friendly
+                filterContext.Controller.TempData["Error"] =
+                    "Tu sesión cambió (abriste otra cuenta en otra pestaña). " +
+                    "Por seguridad, refrescá la página e intentá de nuevo.";
+
+                // Redirigimos a una página segura (ej. Login o Home)
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary(
+                        new { controller = "Account", action = "Login" }
+                    )
+                );
+            }
+            else
+            {
+                base.OnException(filterContext);
+            }
+        }
+    }
+}
